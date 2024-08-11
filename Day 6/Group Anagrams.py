c3f563facbad0857f4bb30c09a10e71367cec6ba
PoojithGanesh <159864736+PoojithGanesh@users.noreply.github.com>
2024-08-11 13:00:44 +0530

    Create Group Anagrams.py

---
+++ Day 6/Group Anagrams.py
@@ -0,0 +1,10 @@
+def group_anagrams(strings):
+    from collections import defaultdict
+    anagrams=defaultdict(list)
+    for s in strings:
+        key=''.join(sorted(s.lower()))
+        anagrams[key].append(s)
+    return list(anagrams.values())
+strings=["Eat", "Tea", "Tan", "Ate", "Nap", "Bat"]
+group_anagrams=group_anagrams(strings)
+print(group_anagrams)
