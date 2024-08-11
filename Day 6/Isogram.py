66698d15d5bb75edde63e2b7b012cda12bbcfa33
PoojithGanesh <159864736+PoojithGanesh@users.noreply.github.com>
2024-08-11 12:55:04 +0530

    Create Isogram.py

---
+++ Day 6/Isogram.py
@@ -0,0 +1,5 @@
+def is_isogram(sentence):
+  cleaned_sentence=sentence.replace(" ", "").lower()
+  return len(cleaned_sentence)==len(set(cleaned_sentence))
+sentence="abcdefghijklmnopqrstuvwxyz"
+print(is_isogram(sentence))
